874c68df0cb8e4665e2fe61d197db642a678ed74
Александр <nikol.alex06@mail.ru>
2025-11-23 14:50:30 +0300

    last commit with xray

--- att.js
+++ att.js
@@ -1,48 +1,143 @@
-const grpc = require('@grpc/grpc-js');
-const protoLoader = require('@grpc/proto-loader');
-const path = require('path');
+const crypto = require('crypto');
 
-// Путь до proto-файла
-const PROTO_PATH = path.join(__dirname, 'proxy', 'proto', 'command.proto');
+const BASE_URL = "http://2.58.65.216:54321/QdV3cAqpJAikqbJ7qx";
+const USERNAME = "hD5Lpo8H4g";
+const PASSWORD = "vYV2StUErt";
+const COOKIE = "3x-ui=MTc2MzgyOTg3M3xEWDhFQVFMX2dBQUJFQUVRQUFCOV80QUFBUVp6ZEhKcGJtY01EQUFLVEU5SFNVNWZWVk5GVWpCbmFYUm9kV0l1WTI5dEwyMW9jMkZ1WVdWcEx6TjRMWFZwTDNZeUwyUmhkR0ZpWVhObEwyMXZaR1ZzTGxWelpYTF9nUU1CQVFSVmMyVnlBZi1DQUFFREFRSkpaQUVFQUFFSVZYTmxjbTVoYldVQkRBQUJDRkJoYzNOM2IzSmtBUXdBQUFCUV80Sk5BUUlCQ21oRU5VeHdiemhJTkdjQlBDUXlZU1F4TUNReGJVSnlTWFZsVTJoeE4zcExOM2xPZWpaaFJtOTFURWhyZFZoUU5FcE5ZazFHVnpBdk9XdzJSMGwyV0RObU1VMWFiRXhOYlFBPXyS9v5-AMUdN6y20JQJwL02g9OqLGa_MJ7C-ZrWaiSY5g=="
 
-// Загружаем определение
-const packageDef = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-});
+async function loginGetCookie() {
+  const res = await fetch(`${BASE_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "User-Agent": "Mozilla/5.0"
+    },
+    body: `username=${encodeURIComponent(USERNAME)}&password=${encodeURIComponent(PASSWORD)}`
+  });
 
-const grpcObj = grpc.loadPackageDefinition(packageDef);
-const handlerService = grpcObj.xray.app.proxyman.command.HandlerService;
+  if (!res.ok) throw new Error("Login failed");
 
-// Подключаемся к Xray API
-const client = new handlerService(
-  '127.0.0.1:9999', // Xray API порт
-  grpc.credentials.createInsecure()
-);
+  // Получаем Set-Cookie из заголовков
+  const rawCookies = res.headers.getSetCookie();
+  if (!rawCookies) throw new Error("No cookies returned");
 
-// Пример нового пользователя
-const newUser = {
-  tag: "api-in", // поменяй на свой inbound tag, например 'inbound-80'
-  user: {
-    email: 'new_user@example.com',
-    level: 0,
-    alterId: 0,
-    account: {
-      id: 'aabbccdd-eeff-1122-3344-556677889900', // UUID
-      flow: '',
-      security: 'auto'
+  // Объединяем куки в одну строку
+  const cookie = rawCookies.map(c => c.split(";")[0]).join("; ");
+
+  console.log("COOKIE:", cookie);
+  return cookie;
+}
+
+async function getUUID() {
+  // const res = await fetch('http://2.58.65.216:54321/QdV3cAqpJAikqbJ7qx/panel/api/server/getNewUUID');
+  // console.log(res);
+  // return await res.json();
+
+  const res = await fetch('http://2.58.65.216:54321/QdV3cAqpJAikqbJ7qx/panel/api/server/getNewUUID', {
+    method: 'GET',
+    headers: {
+      'Cookie': COOKIE
     }
+  });
+  
+  if (res.ok) {
+    return (await res.json())
   }
-};
+}
+// getUUID();
 
-// ➕ Добавляем пользователя
-client.AddUser(newUser, (err, res) => {
-  if (err) {
-    console.error('❌ Ошибка при добавлении пользователя:', err.message);
-  } else {
-    console.log('✅ Пользователь добавлен:', res);
+async function addUser() {
+  try {
+    const res = await getUUID();
+    if (res.success) {
+      const uuid = res.obj.uuid;
+      
+      const subId = crypto.randomBytes(8).toString('hex');
+      const email = crypto.randomBytes(4).toString('hex');
+
+      console.log(JSON.stringify({
+        id: 1,
+        settings: {
+          clients: [
+            {
+              id: uuid,
+              flow: "xtls-rprx-vision",
+              email,
+              limitIp: 0,
+              totalGB: 0,
+              expiryTime: 0,
+              enable: true,
+              tgId: "",
+              subId,
+              comment: "test_user",
+              reset: 0
+            }
+          ]
+        }
+      }));
+
+      const newUser = await fetch('http://2.58.65.216:54321/QdV3cAqpJAikqbJ7qx/panel/api/inbounds/addClient',  {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          'Cookie': COOKIE
+        },
+       body: JSON.stringify({
+        id: 1,
+        settings: JSON.stringify({
+          clients: [
+            {
+              id: uuid,
+              flow: "xtls-rprx-vision",
+              email,
+              limitIp: 0,
+              totalGB: 0,
+              expiryTime: 0,
+              enable: true,
+              tgId: "",
+              subId,
+              comment: "test user",
+              reset: 0
+            }
+          ]
+        })
+      })
+      });
+
+      if (newUser.ok) {
+        console.log(await newUser.json())
+        console.log(`vless://${uuid}@2.58.65.216:443?type=tcp&encryption=none&security=reality&pbk=-k6w2grhneEA0VFavxxRCsnKrAgOisjK8fGBRyEqZg8&fp=chrome&sni=google.com&sid=9adc&spx=%2F&flow=xtls-rprx-vision#admin-${email}`)
+      }
+    } else {
+      throw new Error('Не удалось получить uuid')
+    }
+
+  } catch(e) {
+    console.error(e);
   }
-});
+}
+
+async function deleteUser() {
+  try {
+    const delUser = await fetch('http://2.58.65.216:54321/QdV3cAqpJAikqbJ7qx/panel/api/inbounds/1/delClient/a050d65f-e26d-4536-b6e3-1a530d3e9962',  {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        'Cookie': COOKIE
+      },
+    });
+
+    if (delUser.ok) {
+      console.log(await delUser.json())
+    } else {
+      console.log(delUser.status)
+    }
+
+  } catch(e) {
+    console.error(e);
+  }
+}
+
+// addUser();
+// loginGetCookie();
+deleteUser();
